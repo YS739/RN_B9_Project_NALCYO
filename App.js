d1195322cc083e59ec4b1c1019860b10a29867ce
Leekee01 <zeje23@naver.com>
2023-01-06 22:44:01 +0900

    login css

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
 import Root from "./navigation/Root";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import Login from "./screen/Stacks/Login";
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
-        <Root />
+        {/* <Root /> */}
+        <Login />
       </NavigationContainer>
     </QueryClientProvider>
   );
--- screen/Stacks/Login.jsx
+++ screen/Stacks/Login.jsx
@@ -1,45 +1,48 @@
-import React, { useState } from "react";
-import { Text, StyleSheet, View, TextInput } from "react-native";
-import { Image } from "../assets/adaptive-icon.png";
+import React from "react";
+import { Text, StyleSheet, View, Button, TextInput } from "react-native";
+import { SafeAreaView } from "react-native";
 
 const Login = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.login_title}>오늘 날°C요 </Text>
-      <View>
-        <Image source={Image} />
-      </View>
-      <View>
-        <Text style={styles.email_form_title}>이메일 </Text>
-        <TextInput placeholder="Email" style={styles.login_input} />
-        <TextInput secureTextEntry={true} placeholder="Password" style={styles.login_input} />
-        <Button title="이메일로 로그인하기" onPress={() => onSubmit(form)} style={styles.login_button}></Button>
-        <Button title="회원가입하러가기" onPress={() => navigation.navigate("SignUp")} />
-      </View>
+    <View>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.login_title}>오늘 날°C요 </Text>
+
+        <View>
+          <Text style={styles.email_form_title}>이메일</Text>
+          <TextInput placeholder="Email" style={styles.login_input} />
+          <Text style={styles.email_form_title}>비밀번호</Text>
+          <TextInput secureTextEntry={true} placeholder="Password" style={styles.login_input} />
+          <Button title="이메일로 로그인하기" onPress={() => onSubmit(form)} style={styles.login_button}></Button>
+          <Button title="회원가입 하러가기" onPress={() => navigation.navigate("SignUp")} />
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: (151, 210, 236),
+    alignItems: "center",
+    backgroundColor: "#97D2EC",
   },
   login_title: {
+    padding: 80,
     fontSize: 19,
     fontWeight: "bold",
   },
   email_form_title: {
-    display: flex,
-    alignItems: flex - start,
-    color: black,
     fontSize: 13,
+    padding: 10,
   },
   login_input: {
+    width: 280,
     padding: 24,
-    borderRadius: 4,
+    borderRadius: 30,
+
+    borderWidth: 1,
   },
   login_button: {
-    padding: 24,
     borderRadius: 4,
   },
 });
