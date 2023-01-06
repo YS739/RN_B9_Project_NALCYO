8b30bb4e13695fc31bbc6cd8a5b273cd8dcdc4ca
rhsok823 <rhsok823@gmail.com>
2023-01-06 20:56:34 +0900

    UI
    start

--- screen/Stacks/City.jsx
+++ screen/Stacks/City.jsx
@@ -1,12 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
 const City = () => {
   return (
     <View>
-      <Text>City</Text>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.Weather}>
+            <View></View>
+            <Text>서울</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
 
 export default City;
+
+const styles = StyleSheet.create({
+  Weather: {
+    width: "90%",
+    height: "70%",
+    backgroundColor: "blue",
+  },
+  container: {
+    alignItems: "center",
+  },
+});
