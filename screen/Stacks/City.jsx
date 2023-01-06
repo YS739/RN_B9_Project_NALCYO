8a58d1858ff2c960841cfc104e53f7001b1ee51a
rhsok823 <rhsok823@gmail.com>
2023-01-06 20:14:47 +0900

    aa
    aa

--- screen/Stacks/City.jsx
+++ screen/Stacks/City.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import { SafeAreaView } from "react-native";
+
 const City = () => {
   return (
     <View>
-      <Text>City</Text>
+      <SafeAreaView>
+        <CityContainer>
+          <Text>City</Text>
+        </CityContainer>
+      </SafeAreaView>
     </View>
   );
 };
 
 export default City;
+
+const CityContainer = styled.View`
+  width: 90%;
+`;
