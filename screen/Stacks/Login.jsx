d61dc0478a566f8da93048d08f654121fd8246d9
Leekee01 <zeje23@naver.com>
2023-01-09 19:02:17 +0900

    login navigation.replace home

--- screen/Stacks/Login.jsx
+++ screen/Stacks/Login.jsx
@@ -24,7 +24,9 @@ const Login = ({ navigation }) => {
     firebase
       .auth()
       .signInWithEmailAndPassword(email, pwd)
-      .then(() => {})
+      .then(() => {
+        navigation.replace("Home");
+      })
       .catch((error) => {
         alert(error.message);
         // ..
