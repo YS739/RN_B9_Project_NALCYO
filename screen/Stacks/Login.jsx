70bf3d28253686aa68f8f9315906ae1624be85d4
Leekee01 <zeje23@naver.com>
2023-01-09 19:42:48 +0900

    login firebase

--- screen/Stacks/Login.jsx
+++ screen/Stacks/Login.jsx
@@ -1,4 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { Image, Text, StyleSheet, View, TextInput, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native";
+import firebase from "@firebase/app";
+import "firebase/auth";
+
+const Login = ({ navigation }) => {
+  const [values, setValues] = useState({
+    email: "",
+    pwd: "",
+  });
+
+  function handleChange(text, eventName) {
+    setValues((prev) => {
+      return {
+        ...prev,
+        [eventName]: text,
+      };
+    });
+  }
+
+  function Login() {
+    const { email, pwd } = values;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, pwd)
+      .then(() => {
+        navigation.replace("Home");
+      })
+      .catch((error) => {
+        alert(error.message);
+        // ..
+      });
+  }
+
+  return (
+    <View>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.login_title}>오늘 날°C요 </Text>
+        <Image style={styles.Logo} source={require("../../assets/adaptive-icon.png")} />
+        <View>
+          <Text style={styles.email_form_title}>이메일</Text>
+          <TextInput placeholder="Email" onChangeText={(text) => handleChange(text, "email")} style={styles.login_input} />
+          <Text style={styles.email_form_title}>비밀번호</Text>
+          <TextInput secureTextEntry={true} placeholder="Password" onChangeText={(text) => handleChange(text, "pwd")} style={styles.login_input} />
+          <TouchableOpacity color="#f194ff" onClick={() => Login()} style={styles.login_button}>
+            <Text style={styles.text}>이메일로 로그인하기</Text>
+          </TouchableOpacity>
 
           <TouchableOpacity onPress={() => navigation.navigate("SignUp")} style={styles.login_button}>
             <Text style={styles.text}>회원가입 하러가기</Text>
