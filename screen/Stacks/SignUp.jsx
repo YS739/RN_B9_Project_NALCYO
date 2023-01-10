a73148ef41ca39fb6a0efbf13eed739ae5b23f8c
Leekee01 <zeje23@naver.com>
2023-01-10 10:53:26 +0900

    alert 회원가입성공

--- screen/Stacks/SignUp.jsx
+++ screen/Stacks/SignUp.jsx
@@ -46,7 +46,7 @@ export default function SignUp({ navigation }) {
 
     createUserWithEmailAndPassword(authService, email, pw)
       .then(() => {
-        console.log("성공!");
+        alert("회원가입 성공!");
       })
       .catch((err) => {
         console.log(err.message);
